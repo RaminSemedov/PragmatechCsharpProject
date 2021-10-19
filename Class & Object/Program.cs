2d2fec2b9db113a83bd827b232168a1472b4c42d
RaminSemedov <semedovramin1983@gmail.com>
2021-10-19 13:56:59 +0300

    Task-3  Class & Object

---
+++ Class & Object/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Student
+    {
+        string fullname;
+        string groupNo;
+        int age;
+        public Student(string fullname,string groupNo,int age)
+        {
+            this.fullname = fullname;
+            this.groupNo = groupNo;
+            this.age = age;
+        }
+        public int GetBirthYear()
+        {
+            DateTime date = DateTime.Today;        
+
+            return date.Year-this.age;
+        }
+
+
+    }
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Ramin Semedov","080A4",38);
+            Console.WriteLine(student.GetBirthYear());
+        }
+    }
+}
