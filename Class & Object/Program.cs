3e32177868c78196303413d096dc0398464b21c1
RaminSemedov <semedovramin1983@gmail.com>
2021-10-22 13:02:43 +0300

    #Task - Class & ebjects

--- Class & Object/Program.cs
+++ Class & Object/Program.cs
@@ -4,10 +4,10 @@ namespace ConsoleApp5
 {
     class Student
     {
-        string fullname;
-        string groupNo;
-        int age;
-        public Student(string fullname,string groupNo,int age)
+        public string fullname;
+        public string groupNo;
+        public  int age;
+        public Student(string fullname, string groupNo, int age)
         {
             this.fullname = fullname;
             this.groupNo = groupNo;
@@ -15,9 +15,8 @@ namespace ConsoleApp5
         }
         public int GetBirthYear()
         {
-            DateTime date = DateTime.Today;        
-
-            return date.Year-this.age;
+            DateTime date = DateTime.Today;
+            return date.Year - this.age;
         }
 
 
@@ -30,10 +29,45 @@ namespace ConsoleApp5
 
     class Program
     {
+
+         static int CountStudents(  string groupNumber ,Student[] students)
+        {
+            int sum = 0;
+            
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (groupNumber == students[i].groupNo)
+                    sum++;
+            }
+            return sum;
+        }
         static void Main(string[] args)
         {
-            Student student = new Student("Ramin Semedov","080A4",38);
-            Console.WriteLine(student.GetBirthYear());
+            Student[] students = new Student[2];
+            try
+            {
+                for (int i = 0; i < students.Length; i++)
+                {
+                    Console.WriteLine("Enter Fullname");
+                    string fullname = Console.ReadLine();
+                    Console.WriteLine("Enter Group Number");
+                    string groupNumber = Console.ReadLine();
+                    Console.WriteLine("Enter Age");
+                    int age = Convert.ToInt32(Console.ReadLine());
+
+                    students[i] = new Student(fullname, groupNumber, age);
+                }
+            }catch(Exception)
+            {
+                Console.WriteLine("You entered a wrong data");
+            }
+            
+
+            Console.WriteLine("Enter group number to  get count of students");
+            string groupNo = Console.ReadLine() ;
+            Console.WriteLine($"In group number {groupNo }:study  {CountStudents(groupNo, students)} students");
+
         }
     }
 }
