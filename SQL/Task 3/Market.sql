adb9b30ef236431d5b42577ace5b47093c290bbe
RaminSemedov <semedovramin1983@gmail.com>
2022-01-03 18:01:58 +0300

    Market db

---
+++ SQL/Task 3/Market.sql
@@ -0,0 +1,36 @@
+--CREATE DATABASE Market
+--USE Market
+
+--Create table Product(
+--ID INT IDENTITY NOT NULL,
+--[Name] varchar(20) NOT NULL,
+--Price float NOT NULL
+--)
+
+--Alter table Product
+--Add Brand varchar(50)
+
+--Insert into Product
+--values
+--('Chorek',0.60,'Nomre1'),
+--('Toyuq qanadi',4.89,'Mercan'),
+--('Sosis',7.99,'Kral'),
+--('Sudlu sosis',9.99,'Ostankino'),
+--('Kere yagi',15.40,'Best Cow'),
+--('Yogurt',0.45,'Milla'),
+--('DondurulmushKrevetka',29.9,'Food fish'),
+--('Gunebaxan yagi',4.99,'Yonca')
+
+
+--SELECT * FROM Product
+--Where Price>10
+
+--Select * From Product 
+--where Price<(Select avg(Price) from Product)
+
+--Select  Name  'PrInfo' from Product
+--where LEN([Name])>5
+--union 
+--Select  Brand  'PrInfo' from Product
+--where LEN([Name])>5
+
